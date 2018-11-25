08296e22c0f9ab08d4aad9e4b5d0885b9041941e
Cheng C Yang <Cheng C Yang@cn.asia.ad.pwcinternal.com>
2018-11-25 23:55:08 +0800

    cheng

---
+++ back/src/main/java/com/ethermorgan/onedaycp/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.ethermorgan.onedaycp.controller;
+
+import com.ethermorgan.onedaycp.util.CheckUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginController {
+    @RequestMapping(value = "wx",method= RequestMethod.GET)
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("success");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+                out.write(echostr);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+
+    }
+
+}
